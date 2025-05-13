e3cc0d193c3a19bd490e8a09e295dea00c597748
Alexis Smith <lexlexsq@gmail.com>
2025-05-13 16:42:16 -0400

    Header, animation for buttons

---
+++ Core/Home/Views/HomeView.swift
@@ -0,0 +1,68 @@
+//
+//  SwiftUIView.swift
+//  SwiftTradingAppTest
+//
+//  Created by Alexis Smith on 5/13/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                // .animation(.none) deprecated
+                .transaction { transaction in
+                    transaction.animation = nil
+                }
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices" )
+                // .animation(.none) deprecated
+                .transaction { transaction in
+                    transaction.animation = nil
+                }
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture{
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
--- SwiftTradingAppTestApp.swift
+++ SwiftTradingAppTestApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftTradingAppTestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
