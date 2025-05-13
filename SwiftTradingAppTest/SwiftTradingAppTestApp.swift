ad66cd6f7447d5646863eb177dcde0bfcd087914
Alexis Smith <lexlexsq@gmail.com>
2025-05-13 14:30:37 -0400

    Initial Commit

---
+++ SwiftTradingAppTest/SwiftTradingAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTradingAppTestApp.swift
+//  SwiftTradingAppTest
+//
+//  Created by Alexis Smith on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTradingAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
